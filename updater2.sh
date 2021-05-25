60344254ff8d5a984f5c4cd23fc98f3693609c55
dmginc <coopertgraf@gmail.com>
2021-05-24 21:58:03 -1000

    Update updater2.sh

--- updater2.sh
+++ updater2.sh
@@ -1,4 +1,4 @@
-#Updater v1.0
+#Updater v2.0
 
 #!/bin/bash
 timestamp=`date '+%Y%m%d%H%M%S'`;
@@ -7,8 +7,21 @@ timestamp=`date '+%Y%m%d%H%M%S'`;
 #Backup old whitelist
 /usr/bin/sqlite3 /etc/pihole/gravity.db "SELECT domain FROM domainlist" > /tmp/whitelist.bak.$timestamp;
 
+#Adlist Maintenance
+#Backup old adlist domains
+/usr/bin/sqlite3 /etc/pihole/gravity.db "SELECT address FROM adlist" > /tmp/adlist.bak.$timestamp;
+
 #Delete old whitelist table contents
-/usr/bin/sqlite3 /etc/pihole/gravity.db "DELETE FROM domainlist";
+#/usr/bin/sqlite3 /etc/pihole/gravity.db "DELETE FROM domainlist";
+
+#Output total in new whitelist
+echo `sqlite3 /etc/pihole/gravity.db "SELECT COUNT(domain) FROM domainlist WHERE type = 0;"` "domains in old whitelist"
+
+#Purge old database
+/bin/rm -rf /etc/pihole/gravity.db
+
+#Delete old list data
+/bin/rm -rf /etc/pihole/list.*
 
 #Import new whitelist domains
 for domain in `curl -sS https://raw.githubusercontent.com/dmginc/pihole/master/whitelist.txt`
@@ -17,25 +30,15 @@ id=$((id+1))
 	/usr/bin/sqlite3 /etc/pihole/gravity.db "INSERT OR IGNORE INTO domainlist VALUES ($id, 0, '$domain', 1, date('now'), date ('now'), '');"
 done
 
-#Output total in new whitelist
-echo `sqlite3 /etc/pihole/gravity.db "SELECT COUNT(domain) FROM domainlist WHERE type = 0;"` "domains in new whitelist"
-
-#Adlist Maintenance
-#Backup old adlist domains
-/usr/bin/sqlite3 /etc/pihole/gravity.db "SELECT address FROM adlist" > /tmp/adlist.bak.$timestamp;
-
-#Delete old adlist table contents
-/usr/bin/sqlite3 /etc/pihole/gravity.db "DELETE FROM adlist;"
-
-#Delete old list data
-/bin/rm -rf /etc/pihole/list.*
-
 #Import new adlists
 for address in `curl -sS https://raw.githubusercontent.com/dmginc/pihole/master/adlists.list | grep -v "#"`
 do
 id=$((id+1))
 /usr/bin/sqlite3 /etc/pihole/gravity.db "INSERT OR IGNORE INTO adlist VALUES ($id, '$address', 1, date('now'), date('now'), '', date('now'), '', '', '');"
 done
+
+#Output total in new whitelist
+echo `sqlite3 /etc/pihole/gravity.db "SELECT COUNT(domain) FROM domainlist WHERE type = 0;"` "domains in new whitelist"
 
 #Reload PiHole
 /usr/local/bin/pihole -g
